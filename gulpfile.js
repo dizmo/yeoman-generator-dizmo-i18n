0ccd6eac6f3f0d8e7bff707fda2d3e9db3a58d3d
Hasan Karahan <hasan.karahan@blackhan.com>
2016-07-04 22:17:00 +0400

    DIG-31:: package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp'),
+    gulp_nsp = require('gulp-nsp');
+var path = require('path'),
     require_all = require('require-all');
 
 require_all({
@@ -6,4 +8,9 @@ require_all({
     filter:  /.js$/, recursive: true
 });
 
+gulp.task('nsp', function (callback) {
+    gulp_nsp({package: path.resolve('package.json')}, callback);
+});
+
+gulp.task('prepublish', ['nsp']);
 gulp.task('default', ['install']);
